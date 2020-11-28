cdd1acd5bd5b5dabe26d3c5f57c35cd16bc7d4d3
Alex <alex.y.kuznetsov@gmail.com>
2020-11-28 17:34:47 +0500

    table 1

---
+++ marathon/src/components/UserTable.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="table_cover">
+      <table class="users_table">
+        <thead>
+          <tr class="table_head_row">
+            <th>Name</th>
+            <th>
+              <span>Date of Birth</span>
+              <button class="table_sorting">
+                <span class="material-icons" v-if="sorting.birthSorting.direction === 'descending'">south</span>
+                <span class="material-icons" v-else-if="sorting.birthSorting.direction === 'ascending'">south</span>
+              </button>
+            </th>
+            <th>Email</th>
+            <th>Contact Phone</th>
+            <th>
+              <span>Preferred Distance</span>
+              <button class="table_sorting">
+                <span class="material-icons" v-if="sorting.distanceSorting.direction === 'descending'">south</span>
+                <span class="material-icons" v-else-if="sorting.distanceSorting.direction === 'ascending'">south</span>
+              </button>
+            </th>
+            <th>
+              <span>Donation</span>
+              <button class="table_sorting">
+                <span class="material-icons" v-if="sorting.donationSorting.direction === 'descending'">south</span>
+                <span class="material-icons" v-else-if="sorting.donationSorting.direction === 'ascending'">south</span>
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="table_body_row" v-for="user in users.users" v-bind:key="user.id">
+            <td>{{ user.username }}</td>
+            <td>{{ user.birth }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.phone }}</td>
+            <td>{{ user.distance }}</td>
+            <td>{{ user.donation }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+import users from '@/data/users.json'
+export default {
+  data () {
+    return {
+      users,
+      sorting: {
+        birthSorting: {
+          bool: true,
+          direction: 'descending'
+        },
+        distanceSorting: {
+          bool: false,
+          direction: 'descending'
+        },
+        donationSorting: {
+          bool: false,
+          direction: 'descending'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .table_cover {
+    padding: 50px;
+  }
+  .users_table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  .table_head_row {
+    background-color: #d1fdfd;
+  }
+  th, td {
+    text-align: left;
+    padding: 10px;
+    border: 1px solid #867e7e;
+  }
+  th {
+    white-space: nowrap;
+  }
+  .table_sorting {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+  .table_sorting:focus {
+    outline: none;
+  }
+  .table_sorting:focus .material-icons {
+    color: #f72d8a;
+  }
+  .table_sorting .material-icons {
+    font-size: 16px;
+    vertical-align: middle;
+  }
+</style>
--- marathon/src/views/Home.vue
+++ marathon/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Form />
+    <UserTable />
   </div>
 </template>
 
 <script>
 import Form from '@/components/Form.vue'
+import UserTable from '@/components/UserTable.vue'
 
 export default {
   name: 'Home',
   components: {
-    Form
+    Form,
+    UserTable
   }
 }
 </script>
